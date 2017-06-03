e01ed7a9657d3d86f0f32e4b6dc16b20fc201d46
malloc1432 <malloc1432@gmail.com>
2017-06-03 20:56:31 +0900

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour {
+
+    public float speed;
+    PlayerController controller;
+    GunController gunController;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * speed;
+        controller.Move(moveVelocity);
+
+        //Weapon Input
+        if (Input.GetMouseButton(0)) {
+            gunController.Shoot();
+        }
+    }
+}
